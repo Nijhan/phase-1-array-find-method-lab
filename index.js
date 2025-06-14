14964c3f9f17cbf2affd5cf02f1c90ed99543719
Nijhan <najmashaaban6@gmail.com>
2025-06-15 01:33:13 +0300

    array-find-method-lab

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // code your solution here
+function superbowlWin(record){
+    const win =record.find(game=> game.result==="W");
+    return win ?win.year:undefined;
+}
+//Example usage;
+const broncosRecord =[
+    {year:2013, team:"Denver Broncos", result:"L"},
+    {year: 2015, team:"Denver Broncos", result:"W"},
+    {year:2017, team:"Denver Broncos", result:"L"}
+
+];
+console.log(superbowlWin(broncosRecord)); //output: 2015
